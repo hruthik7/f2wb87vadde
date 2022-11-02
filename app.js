c03bdae1b36387e3a4d692f82f908db0cf271029
Hruthik Reddy Vadde <112142362+hruthik7@users.noreply.github.com>
2022-11-02 11:20:51 -0500

    updated grid build pug file.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var rabbitRouter = require('./routes/rabbit');
+var gridbuildRouter = require('./routes/gridbuild');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/rabbit', rabbitRouter);
+app.use('/rabbit', gridbuildRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
