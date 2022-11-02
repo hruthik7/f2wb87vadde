318897b2550918c6ec9952e79a6e9a2761017b0d
Hruthik Reddy Vadde <112142362+hruthik7@users.noreply.github.com>
2022-11-02 11:40:18 -0500

    updated gridbuild files and app.js

--- routes/gridbuild.js
+++ routes/gridbuild.js
@@ -4,9 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let query = req.query
-console.log(`rows ${query.rows}`)
-console.log(`cols ${query.cols}`)
-  res.render('index', { title: 'Grid Build', query: query });
+  console.log(`rows ${query.rows}`)
+  console.log(`cols ${query.cols}`)
+  res.render('gridbuild', { title: 'Grid Build', query: query });
 });
 
 module.exports = router;
