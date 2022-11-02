936fc46efba1b7eec9a0cba02cd9b30ddc8663b0
Hruthik Reddy Vadde <112142362+hruthik7@users.noreply.github.com>
2022-11-02 11:00:37 -0500

    updated rabbit files

--- routes/rabbit.js
+++ routes/rabbit.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+class rabbit{
+  constructor(rabbit_Name,rabbit_Price,rabbit_gender){
+    this.rabbit_Name = rabbit_Name;
+    this.rabbit_Price = rabbit_Price;
+    this.rabbit_gender = rabbit_gender;
+
+}
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('rabbit', { title: 'Search Results rabbit' });
+  let rabbitone = new rabbit('mike', 7000, 'male');
+  let rabbittwo = new rabbit('elena', 16000, 'female');
+  let rabbitthree = new rabbit('stefan', 34000, 'male');
+
+  res.render('rabbit', { title: 'Search Results rabbit',rabbit : [rabbitone,rabbittwo,rabbitthree] });
 });
 
 module.exports = router;
